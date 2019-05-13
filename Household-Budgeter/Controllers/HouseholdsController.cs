dfb09c259a1f920b10032830668c706201bb6813
Jashan Saraon <Jashanp.k99@gmail.com>
2019-05-13 12:58:30 -0500

    Add project files.

---
+++ Household-Budgeter/Controllers/HouseholdsController.cs
@@ -0,0 +1,58 @@
+﻿using Household_Budgeter.Models;
+using Household_Budgeter.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Household_Budgeter.Controllers
+{
+    public class HouseholdsController : ApiController
+    {
+        private ApplicationDbContext DbContext;
+        public HouseholdsController()
+        {
+            DbContext = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult Get(int? id)
+        {
+            var allhouseholds = DbContext.HouseholdManagement.FirstOrDefault(user => user.Id == id);
+            if (allhouseholds == null)
+            {
+                return NotFound();
+            }
+            return Ok(allhouseholds);
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(DbContext.HouseholdManagement);
+        }
+
+        public IHttpActionResult Post(HouseholdsViewModel formdata)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Households households = new Households();
+            households.Name = formdata.Name;
+            households.Description = formdata.Description;
+            DbContext.HouseholdManagement.Add(households);
+            DbContext.SaveChanges();
+            return Ok(formdata);
+        }
+
+        public IHttpActionResult Put(int? id)
+        {
+            var allhouseholds = DbContext.HouseholdManagement.FirstOrDefault(p => p.Id == id);
+            var model = new HouseholdsViewModel();
+            model.Name = allhouseholds.Name;
+            model.Description = allhouseholds.Description;              
+            return Ok(model);
+        }       
+    }
+}
---
+++ Household-Budgeter/Models/Domain/Categories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Household_Budgeter.Models.Domain
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public Categories()
+        {
+            DateUpdated = DateTime.Now;
+        }
+    }
+}
